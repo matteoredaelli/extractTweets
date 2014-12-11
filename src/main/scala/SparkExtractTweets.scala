ef75de3675bebbbee1ee31b14c6c8020c4dface4
matteoredaelli <matteo.redaelli@gmail.com>
2014-12-11 17:25:44 +0100

    added top hashtags and words (todo stopwords)

--- src/main/scala/SparkExtractTweets.scala
+++ src/main/scala/SparkExtractTweets.scala
@@ -13,6 +13,18 @@ object SparkExtractTweets {
     println(s"<a href=https://twitter.com/$user/status/$id>$date</a>: $text<br />");
   }
 
+/*
+  t0(0).asInstanceOf[scala.collection.mutable.ArrayBuffer[String]].getClass)
+
+  def extractTags(hashtagEntity: Array[String]) : String = {
+     if( hashtagEntities.asInstanceOf[scala.collection.mutable.ArrayBuffer[Any].length > 3) {
+        return hashtagEntity(2) 
+     } else {
+        return ""
+     }
+  }
+*/
+
   def main(args: Array[String]) {
     val sparkConf = new SparkConf().setAppName("RDDRelation")
     val sc = new SparkContext(sparkConf)
@@ -29,13 +41,22 @@ object SparkExtractTweets {
 
     var extractedTweets = t.map(t0 => (ft.format(new java.util.Date(t0(0).asInstanceOf[Long])), t0(1), t0(2), t0(3))).distinct()
 
+    var words = t.flatMap(t0 => "[\n\t.;:?]".r.replaceAllIn(t0(3).toString, " ").toLowerCase().split(" "))
+    var topWords = words.map(t0 => (t0,1)).reduceByKey(_ + _).map(t0 => (t0._2,t0._1)).sortByKey(ascending=false).take(10).map(t0 => (t0._2,t0._1))
+    var topHashTags = words.filter(word => word.startsWith("#")).map(t0 => (t0,1)).reduceByKey(_ + _).map(t0 => (t0._2,t0._1)).sortByKey(ascending=false).take(10).map(t0 => (t0._2,t0._1))
+
     println(s"<html><head><title>$title</title></head>")
     println("<body>")
     println(s"<h1>$title</h1>")
     println("<h2>Tweets</h2>")
     extractedTweets.collect().foreach(tweet2Html)
     println("<h2>Stats</h2>")
+    println("<h3>Tweets count</h3>")
     t.map(t0 => (ft.format(new java.util.Date(t0(0).asInstanceOf[Long])), 1)).reduceByKey(_ + _).sortByKey().collect().foreach(println)
+    println("<h3>Top hashtags</h3>")
+    topHashTags.foreach(println)
+    println("<h3>Top words</h3>")
+    topWords.foreach(println)
 
     println("</body>")
     println("<html>")
