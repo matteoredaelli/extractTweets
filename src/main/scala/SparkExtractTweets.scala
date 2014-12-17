b4ae608847bc2245dada2fd8be114e728205d925
matteoredaelli <matteo.redaelli@gmail.com>
2014-12-17 16:03:33 +0100

    output to files

--- src/main/scala/SparkExtractTweets.scala
+++ src/main/scala/SparkExtractTweets.scala
@@ -6,60 +6,66 @@ import java.text.SimpleDateFormat
 import java.text.DateFormat
 import java.util.Date
 
+import java.io.PrintWriter
+
 object SparkExtractTweets {
 
-  def tweet2Html(tweet:(Any,Any,Any,Any)) {
-    val (date, user, id, text) = tweet
-    println(s"<a href=https://twitter.com/$user/status/$id>$date</a>: $text<br />");
+  def cleanText(text: Any) : String = {
+    val t1 = "[\n\t]".r.replaceAllIn(text.toString, " ")
+    val t2 = "[:?!.]$".r.replaceAllIn(t1, "")
+    return t2;
   }
 
-/*
-  t0(0).asInstanceOf[scala.collection.mutable.ArrayBuffer[String]].getClass)
-
-  def extractTags(hashtagEntity: Array[String]) : String = {
-     if( hashtagEntities.asInstanceOf[scala.collection.mutable.ArrayBuffer[Any].length > 3) {
-        return hashtagEntity(2) 
-     } else {
-        return ""
-     }
+  def formatTweet(row:(Any,Any,Any,Any)) : String = {
+    val (createdAt, user, id, text) = row
+    return s"$createdAt\t$user\t$id\t$text";
   }
-*/
+
+  def formatTopRecord(row:(Any,Any)) : String = {
+    val (text, count) = row
+    return s"$text\t$count";
+  }
+
+  def saveArrayToFile(array: Array[String], filename: String) {
+    val s = new PrintWriter(filename)
+    array.foreach(s.println)
+    s.close()
+  }
 
   def main(args: Array[String]) {
-    val sparkConf = new SparkConf().setAppName("RDDRelation")
+    val sparkConf = new SparkConf().setAppName("Twitter-extractTweets")
     val sc = new SparkContext(sparkConf)
     val sqlContext = new SQLContext(sc)
-    val filename = "staging/twitter/" + args(0) // staging/twitter/searches/opensource/2014/12/*
-    val title = args(0) // 
-    val tweets = sqlContext.jsonFile(filename)
+    val source_path = args(0)
+    val target_path = args(1)
 
+    val tweets = sqlContext.jsonFile(source_path)
     tweets.registerTempTable("tweets")
 
-    val t = sqlContext.sql("SELECT distinct createdAt, user.name, id, text FROM tweets")
+    val t = sqlContext.sql("SELECT distinct createdAt, user.screenName, id, text FROM tweets")
 
     var ft = new java.text.SimpleDateFormat("yyyy-MM-dd");
 
     var extractedTweets = t.map(t0 => (ft.format(new java.util.Date(t0(0).asInstanceOf[Long])), t0(1), t0(2), t0(3))).distinct()
 
-    var words = t.flatMap(t0 => "[\n\t.;:?]".r.replaceAllIn(t0(3).toString, " ").toLowerCase().split(" "))
-    var topWords = words.map(t0 => (t0,1)).reduceByKey(_ + _).map(t0 => (t0._2,t0._1)).sortByKey(ascending=false).take(10).map(t0 => (t0._2,t0._1))
-    var topHashTags = words.filter(word => word.startsWith("#")).map(t0 => (t0,1)).reduceByKey(_ + _).map(t0 => (t0._2,t0._1)).sortByKey(ascending=false).take(10).map(t0 => (t0._2,t0._1))
+    var words = t.flatMap(t0 => "[\n\t]".r.replaceAllIn(t0(3).toString, " ").toLowerCase().split(" "))
+    var topHashTags = words.filter(word => word.startsWith("#")).map(t0 => (cleanText(t0),1)).reduceByKey(_ + _).map(t0 => (t0._2,t0._1)).sortByKey(ascending=false).take(10).map(t0 => (t0._2,t0._1))
+    var topMentions = words.filter(word => word.startsWith("@")).map(t0 => (cleanText(t0),1)).reduceByKey(_ + _).map(t0 => (t0._2,t0._1)).sortByKey(ascending=false).take(10).map(t0 => (t0._2,t0._1))
 
-    println(s"<html><head><title>$title</title></head>")
-    println("<body>")
-    println(s"<h1>$title</h1>")
-    println("<h2>Tweets</h2>")
-    extractedTweets.collect().foreach(tweet2Html)
+    var topUsers = t.map(t0 => (cleanText(t0(1)),1)).reduceByKey(_ + _).map(t0 => (t0._2,t0._1)).sortByKey(ascending=false).take(10).map(t0 => (t0._2,t0._1))
+
+    // saving tweets
+    
+    saveArrayToFile(extractedTweets.map(t0 => formatTweet(t0)).collect(), target_path + "/tweets.tsv")
+
+    saveArrayToFile(topHashTags.map(r => formatTopRecord(r)), target_path + "/top-hashtags.tsv")
+    saveArrayToFile(topMentions.map(r => formatTopRecord(r)), target_path + "/top-mentions.tsv")
+    saveArrayToFile(topUsers.map(r => formatTopRecord(r)), target_path + "/top-users.tsv")
+
     println("<h2>Stats</h2>")
     println("<h3>Tweets count</h3>")
     t.map(t0 => (ft.format(new java.util.Date(t0(0).asInstanceOf[Long])), 1)).reduceByKey(_ + _).sortByKey().collect().foreach(println)
-    println("<h3>Top hashtags</h3>")
-    topHashTags.foreach(println)
-    println("<h3>Top words</h3>")
-    topWords.foreach(println)
 
-    println("</body>")
-    println("<html>")
     sc.stop()
   }
 }
