3b14d695e1001da62684aa2a7c0355630be63fbc
matteoredaelli <matteo.redaelli@gmail.com>
2014-12-19 14:43:04 +0100

    option for skipping tweet list

--- r-scripts/tweets-report-builder.R
+++ r-scripts/tweets-report-builder.R
@@ -8,6 +8,9 @@ if (length(args) < 3) stop("Bad args, usage title input output")
 title <- args[1]
 source_path <- args[2]
 target_path <- args[3]
+skip.tweets <- ifelse(is.na(args[4]), FALSE, TRUE)
+
+print(skip.tweets)
 
 template_file = "tweets-report-template.Rhtml"
 
