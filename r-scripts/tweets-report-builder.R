120ec27f9e0f474706277b73925906f865133cba
matteoredaelli <matteo.redaelli@gmail.com>
2014-12-19 11:37:35 +0100

    added html reports in R

---
+++ r-scripts/tweets-report-builder.R
@@ -0,0 +1,17 @@
+#!/usr/bin/env Rscript
+
+library(knitr)
+
+args <- commandArgs(TRUE)
+if (length(args) < 3) stop("Bad args, usage title input output")
+
+title <- args[1]
+source_path <- args[2]
+target_path <- args[3]
+
+template_file = "tweets-report-template.Rhtml"
+
+page = readChar(template_file, file.info(template_file)$size)
+page = gsub("__TITLE__", title, page)
+
+knit(text=page, output=target_path)
--- src/main/scala/SparkExtractTweets.scala
+++ src/main/scala/SparkExtractTweets.scala
@@ -10,6 +10,11 @@ import java.io.PrintWriter
 
 object SparkExtractTweets {
 
+  def extractSourceText(text: Any) : String = {
+    //val placeholder = new Regex("""<a href=.+>(.+)</a>""")
+    return "<a href=.+>(.+)</a>".r.replaceAllIn(text.toString, "$1");
+  }
+
   def cleanText(text: Any) : String = {
     val t1 = "[\n\t]".r.replaceAllIn(text.toString, " ")
     val t2 = "[:?!.]$".r.replaceAllIn(t1, "")
@@ -38,33 +43,35 @@ object SparkExtractTweets {
     val sqlContext = new SQLContext(sc)
     val source_path = args(0)
     val target_path = args(1)
+    val top = 20
 
     val tweets = sqlContext.jsonFile(source_path)
     tweets.registerTempTable("tweets")
 
-    val t = sqlContext.sql("SELECT distinct createdAt, user.screenName, id, text FROM tweets")
+    val t = sqlContext.sql("SELECT distinct createdAt, user.screenName, id, text, source FROM tweets")
 
     var ft = new java.text.SimpleDateFormat("yyyy-MM-dd");
 
     var extractedTweets = t.map(t0 => (ft.format(new java.util.Date(t0(0).asInstanceOf[Long])), t0(1), t0(2), cleanText(t0(3)))).distinct()
 
     var words = t.flatMap(t0 => "[\n\t]".r.replaceAllIn(t0(3).toString, " ").toLowerCase().split(" "))
-    var topHashTags = words.filter(word => word.startsWith("#")).map(t0 => (cleanText(t0),1)).reduceByKey(_ + _).map(t0 => (t0._2,t0._1)).sortByKey(ascending=false).take(10).map(t0 => (t0._2,t0._1))
-    var topMentions = words.filter(word => word.startsWith("@")).map(t0 => (cleanText(t0),1)).reduceByKey(_ + _).map(t0 => (t0._2,t0._1)).sortByKey(ascending=false).take(10).map(t0 => (t0._2,t0._1))
+    var topHashTags = words.filter(word => word.startsWith("#")).map(t0 => (cleanText(t0),1)).reduceByKey(_ + _).map(t0 => (t0._2,t0._1)).sortByKey(ascending=false).take(top).map(t0 => (t0._2,t0._1))
+    var topMentions = words.filter(word => word.startsWith("@")).map(t0 => (cleanText(t0),1)).reduceByKey(_ + _).map(t0 => (t0._2,t0._1)).sortByKey(ascending=false).take(top).map(t0 => (t0._2,t0._1))
 
-    var topUsers = t.map(t0 => (cleanText(t0(1)),1)).reduceByKey(_ + _).map(t0 => (t0._2,t0._1)).sortByKey(ascending=false).take(10).map(t0 => (t0._2,t0._1))
+    var topUsers = t.map(t0 => (cleanText(t0(1)),1)).reduceByKey(_ + _).map(t0 => (t0._2,t0._1)).sortByKey(ascending=false).take(top).map(t0 => (t0._2,t0._1))
 
+    var tweetsByDays = t.map(t0 => (ft.format(new java.util.Date(t0(0).asInstanceOf[Long])), 1)).reduceByKey(_ + _).sortByKey().collect()
     // saving tweets
     
+    var topSources = t.map(t0 => (extractSourceText(t0(4)),1)).reduceByKey(_ + _).map(t0 => (t0._2,t0._1)).sortByKey(ascending=false).take(top).map(t0 => (t0._2,t0._1))
+
     saveArrayToFile(extractedTweets.map(t0 => formatTweet(t0)).collect(), target_path + "/tweets.tsv")
 
     saveArrayToFile(topHashTags.map(r => formatTopRecord(r)), target_path + "/top-hashtags.tsv")
     saveArrayToFile(topMentions.map(r => formatTopRecord(r)), target_path + "/top-mentions.tsv")
     saveArrayToFile(topUsers.map(r => formatTopRecord(r)), target_path + "/top-users.tsv")
-
-    println("<h2>Stats</h2>")
-    println("<h3>Tweets count</h3>")
-    t.map(t0 => (ft.format(new java.util.Date(t0(0).asInstanceOf[Long])), 1)).reduceByKey(_ + _).sortByKey().collect().foreach(println)
+    saveArrayToFile(tweetsByDays.map(r => formatTopRecord(r)), target_path + "/tweets-by-day.tsv")
+    saveArrayToFile(topSources.map(r => formatTopRecord(r)), target_path + "/top-sources.tsv")
 
     sc.stop()
   }
